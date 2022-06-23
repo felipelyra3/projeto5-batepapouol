f7223d5b3e1f04165a3078dc33bae167308e7f86
felipelyra3 <felipelyra3@gmail.com>
2022-06-22 21:12:49 -0300

    2022-06-22_Layout1.0

---
+++ script.js
@@ -0,0 +1,6 @@
+function entrar() {
+    const telaDeEntrada = document.querySelector('.telaDeEntrada');
+    const containerChat = document.querySelector('.containerChat');
+    telaDeEntrada.classList.add('hide');
+    containerChat.classList.remove('displayNone');
+}
